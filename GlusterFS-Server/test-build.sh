1aea56d5eec08149fac758daa4512816f1eee4ad
GoingOffRoading <CWestlye@gmail.com>
2021-10-17 17:30:35 -0700

    Updates to the build process

--- GlusterFS-Server/test-build.sh
+++ GlusterFS-Server/test-build.sh
@@ -2,6 +2,6 @@
 
 sudo docker build --no-cache -t glusterserver . 
 
-sudo docker containers ls | grep -e gluster
+sudo docker container ls | grep -e 'CONTAINER\|gluster'
 
 echo "Next Step: sudo docker run ––name CONTAINER-NAME –d nginx"
