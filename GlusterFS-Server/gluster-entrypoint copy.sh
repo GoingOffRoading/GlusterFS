2e6b5efcee4a90de2666d024760bac61d9f872c7
GoingOffRoading <CWestlye@gmail.com>
2021-10-18 21:29:53 -0700

    Trying CentOS as the Docker Image for Gluster

---
+++ GlusterFS-Server/gluster-entrypoint copy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Needed to start the Gluster Daemon in the container
+glusterd --no-daemon --log-file=/dev/stdout 
--- GlusterFS-Server/gluster-entrypoint.sh
+++ GlusterFS-Server/gluster-entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#Needed to start the Gluster Daemon in the container
-glusterd --no-daemon --log-file=/dev/stdout 
+glusterd
